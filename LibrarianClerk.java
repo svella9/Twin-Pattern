69f8271346e9f5fed6f2241603b1962d2ee84582
svella9 <svella9@gmail.com>
2017-12-07 22:20:14 +0530

    Updated LibrarianClerk functionlities

--- LibrarianClerk.java
+++ LibrarianClerk.java
@@ -1,38 +1,65 @@
 class LibrarianClerk extends LibraryClerk
 {
+	Library lib;
 	LibrarianStaff twin;
+
 	LibrarianClerk()
 	{
+		lib = Library.getInst();
 	}
 	
-	String getUserDetails()
+	void getUserDetails(String userid)
 	{
-		return "";
+		if( lib.userTable.containsKey(userid) )
+		{
+			User u = lib.userTable.get(userid);
+			System.out.println("User Name: " + u.name);
+			System.out.println("Items borrowed: ");
+			for(int i = 0; i < u.itemlist.size(); ++i)
+			{
+				System.out.println("\t " + u.itemlist.get(i));
+			}
+		}
+		else
+		{
+			System.out.println("User does not exists..");
+		}
+
 	}
 	
-	String getItemDetails()
+	void getItemDetails(String itemid)
 	{
-		return "";
+		if( lib.itemTable.containsKey(itemid) )
+		{
+			Item it = lib.itemTable.get(itemid);
+			System.out.println("Item Name: " + it.name);
+			System.out.println("No. of copies: " + it.no_of_copy);
+		}
+		else
+		{
+			System.out.println("Item does not exists..");
+		}
 	}
 	
-	boolean updateUserDetails()
+	void updateUserDetails()
 	{
-		return false;
 	}
 	
-	boolean updateItemDetails()
+	void updateItemDetails()
 	{
-		return false;
 	}
-	
-	void addPolicy()
-	{
 		
-	}
-	
-	void waiveFine(String user)
+	void waiveFine(String userid)
 	{
-		
+		if( lib.userTable.containsKey(userid) )
+		{
+			lib.userTable.get(userid).fine = 0;
+			System.out.println("Fine waived..");
+		}
+		else
+		{
+			System.out.println("User does not exists..");
+		}
 	}
 	
 }
--- User.java
+++ User.java
@@ -5,11 +5,13 @@ class User
 	String id;
 	String name;
 	ArrayList<String> itemlist;
+	int fine;
 
 	User(String id, String name)
 	{
 		this.id = id;
 		this.name = name;
 		this.itemlist = new ArrayList<>();
+		this.fine = 0;
 	}
 }
